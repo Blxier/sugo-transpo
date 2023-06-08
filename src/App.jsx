673d8f0a040d6268c5c48986baae040f42fdf636
JR_RUEL <blxier.odaroma@gmail.com>
2023-06-08 09:43:42 +0800

    Tutorial Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Food from "./components/Food";
+import Category from "./components/Category";
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
     </div>
   );
 }
